078f6dd0fdb748c703e11e5f99ae92c86715562d
Jarrett Dougherty <jarrettdougherty@gmail.com>
2020-05-17 18:02:33 -0400

    set up repo tree

---
+++ assets/javascript/script.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDMzGY5EYrp70hJfi-_mg_nQvEHU4avs18",
+    authDomain: "test-project-f0ef0.firebaseapp.com",
+    databaseURL: "https://test-project-f0ef0.firebaseio.com",
+    projectId: "test-project-f0ef0",
+    storageBucket: "test-project-f0ef0.appspot.com",
+    messagingSenderId: "691526510764",
+    appId: "1:691526510764:web:45298f0751376ef02ad709"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
